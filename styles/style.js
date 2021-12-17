13bf6f9cfcd59159d2394e4b9115719919ada7db
Abror Mamataliev <abrornomos2018@gmail.com>
2021-12-18 00:23:51 +0500

    Initial setup

---
+++ styles/style.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native-web";
+
+export const gStyle = StyleSheet.create({
+  main: {
+    flex: 1,
+    padding: 20
+  },
+  title: {
+    fontSize: 20,
+    color: "#333",
+    fontFamily: 'mt-bold'
+  }
+})
